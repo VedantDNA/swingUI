5234f3c4956e06f228a7968930ed78af01c4a2cd
Vedant A <dev_vedant@Vedants-Mac-mini.local>
2026-04-09 06:51:11 +0530

    added combo box

--- src/examprep/LearnComboBox.java
+++ src/examprep/LearnComboBox.java
@@ -1,4 +1,43 @@
 package examprep;
 
-public class LearnComboBox {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LearnComboBox extends JFrame implements ActionListener {
+
+    JComboBox<String> cityBox;
+    JLabel outputLabel;
+
+    public LearnComboBox(String title){
+        String[] cities = {"Pune","Mumbai","Bangalore","Chennai"};
+        outputLabel = new JLabel("");
+
+        setSize(400,400);
+        setLayout(new FlowLayout());
+
+        cityBox = new JComboBox<>(cities);
+        cityBox.setSelectedIndex(0);
+
+        cityBox.addActionListener(this);
+
+        cityBox.addItem("Himachal");
+        cityBox.removeItem("Himachal");
+
+        add(cityBox);
+        add(outputLabel);
+
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String selected = (String) cityBox.getSelectedItem();
+        outputLabel.setText("Cities: " + selected);
+    }
+
+    public static void main(String[] args) {
+        new LearnComboBox("Combo Box Tutorial");
+    }
 }
